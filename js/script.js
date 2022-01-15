e77947406ee2736b797ac5f98ea60f5fbe5f2530
Abid Al Wassie <abidalwassie@outlook.com>
2022-01-15 19:10:07 +0600

    major changes

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// window.onload = function() {
+
+  // window.addEventListener("scroll", function() {
+  //   const navbar = document.getElementById("navbar");
+  //   navbar.classList.toggle("sticky", window.scrollY > 0);
+  // });
+
+  
+  function optToggle() {
+    const optMenu = document.querySelector("#optMenu");
+    optMenu.classList.toggle("hidden");
+  }
+  
+  function navToggle() {
+    const navMenu = document.querySelector("#navMenu");
+    navMenu.classList.toggle("fh");
+  }
+// }
